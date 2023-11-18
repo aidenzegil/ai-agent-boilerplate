2fb87ce8dfbd4ed63d661cc72e336c6620b22af7
jordon <jordon@tenet.com>
2023-11-18 08:05:25 -0500

    add controllers

--- api-v1/src/app.module.ts
+++ api-v1/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
+import { UsersController } from './controllers/users/users.controller';
+import { StoriesController } from './controllers/stories/stories.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UsersController, StoriesController],
   providers: [AppService],
 })
 export class AppModule {}
