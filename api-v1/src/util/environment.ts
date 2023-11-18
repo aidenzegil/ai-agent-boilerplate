ed71eab1b7d93ca0dde5fe6342eb38d51d68ce08
jordon <jordon@tenet.com>
2023-11-18 10:26:59 -0500

    the database is wet

---
+++ api-v1/src/util/environment.ts
@@ -0,0 +1,20 @@
+const getEnvironmentName = () => {
+  const environmentName = process.env.ENV;
+  if (environmentName === undefined) {
+    throw new Error("Environment name cannot be undefined: process.env");
+  }
+  return environmentName;
+};
+export const isProduction = () => {
+  return getEnvironmentName() === "prod";
+};
+
+const isLocal = () => {
+  return getEnvironmentName() === "local";
+};
+
+export const environment = {
+  name: getEnvironmentName(),
+  isProduction,
+  isLocal,
+};
