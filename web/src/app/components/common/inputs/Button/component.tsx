dfdb9766112ae612fa21eaca5cbf827400634586
Aiden Zegil <aidenzegil@gmail.com>
2023-11-24 17:22:06 -0500

    create base button component

---
+++ web/src/app/components/common/inputs/Button/component.tsx
@@ -0,0 +1,42 @@
+import { ButtonProps, ButtonSize, ButtonVariant } from "./types";
+import s from "./styles.module.scss";
+import classNames from "classnames";
+
+const Button = ({ size, variant, children, onClick }: ButtonProps) => {
+  const sizeClassName = (() => {
+    switch (size) {
+      case ButtonSize.LARGE:
+        return s.large;
+      case ButtonSize.MEDIUM:
+        return s.medium;
+      case ButtonSize.SMALL:
+        return s.small;
+      default:
+        return s.medium;
+    }
+  })();
+
+  const variantClassName = (() => {
+    switch (variant) {
+      case ButtonVariant.PRIMARY_FILLED:
+        return s.primaryFilled;
+      case ButtonVariant.PRIMARY_OUTLINED:
+        return s.primaryOutlined;
+      case ButtonVariant.SUDO_LINK:
+        return s.sudoLink;
+      default:
+        return s.primaryFilled;
+    }
+  })();
+
+  return (
+    <button
+      className={classNames(s.base, sizeClassName, variantClassName)}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
