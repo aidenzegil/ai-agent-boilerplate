357d44b36388aa0573575875fc5d13c3d11eaa91
Aiden Zegil <aidenzegil@gmail.com>
2023-12-26 11:10:27 -0500

    add textearea and input common components

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import Input from "./components/common/inputs/Input/component";
+import TextArea from "./components/common/inputs/TextArea/component";
+import LeafCard from "./components/common/surfaces/LeafCard/component";
+
 export default function Home() {
-  return <main></main>;
+  return <main style={{ height: "100vh", width: "100%" }}></main>;
 }
