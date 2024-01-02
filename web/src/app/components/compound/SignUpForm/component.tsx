548363d00cc351f699c4b94554ea3af46b06d5e9
Samuel Crider <sam.crider@aol.com>
2024-01-02 17:33:52 -0500

    signup form component file cleaned

--- web/src/app/components/compound/SignUpForm/component.tsx
+++ web/src/app/components/compound/SignUpForm/component.tsx
@@ -1,10 +1,10 @@
-import React, { forwardRef, useEffect, useRef } from "react";
+import React from "react";
 import { Fields } from "./types";
 import s from "./styles.module.scss";
 import { LuBook } from "react-icons/lu";
-
 import Button from "../../common/inputs/Button/component";
 import Input from "../../common/inputs/Input/component";
+import Form from "../../common/forms/SignUp/component";
 
 const Component = ({ onSubmit, form }: Fields) => {
   const { register } = form;
@@ -25,66 +25,5 @@ const Component = ({ onSubmit, form }: Fields) => {
     </div>
   );
 };
-
-/**
- 
-Form is a simple wrapper around an HTML <form> element, with a set of props
-designed for use with "react-hook-form". More specifically:
-*
-The form component contains styles for arranging inputs vertically with a
-standard gap amount.
-The form component will redirect focus to the first focusable element in
-the Form after mounting.
-The form component will handle rendering form-level errors when a
-"react-hook-form" errors object is passed with a error key with the
-name "form".
-The form component will handle clearing "form" errors on submit events,
-when a "react-hook-form" clearErrors fn is passed.
-*/
-interface Props extends React.ComponentPropsWithRef<"form"> {
-  clearErrors?: (name?: "form") => void;
-  errors?: { [N: "form" | string]: { message?: string } | undefined };
-}
-
-const Form = forwardRef<HTMLFormElement, Props>(
-  (
-    {
-      children,
-      clearErrors,
-      onSubmit,
-      errors,
-      autoFocus = true,
-      ...otherProps
-    },
-    ref
-  ) => {
-    const formRef = useRef<HTMLFormElement>(null);
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      clearErrors?.("form");
-      e.preventDefault();
-      onSubmit?.(e);
-    };
-
-    useEffect(() => {
-      // If a ref value has been forwarded, we need to share current
-      // formRef value with it on every render.
-      if (formRef && ref) {
-        if (typeof ref === "function") {
-          ref(formRef.current);
-        } else {
-          // eslint-disable-next-line no-param-reassign
-          ref.current = formRef.current;
-        }
-      }
-    });
-
-    return (
-      <form onSubmit={handleSubmit} ref={formRef} {...otherProps}>
-        {errors?.form && errors.form.message}
-        {children}
-      </form>
-    );
-  }
-);
 
 export default Component;
