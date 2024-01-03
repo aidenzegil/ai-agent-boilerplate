b0d4ba8a9f511eb856025abbc2c914063849c038
Samuel Crider <sam.crider@aol.com>
2024-01-02 21:41:04 -0500

    resolved all issues with signup form

--- web/src/app/components/common/inputs/Input/component.tsx
+++ web/src/app/components/common/inputs/Input/component.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import s from "./styles.module.scss";
 import { InputProps } from "./types";
+import classNames from "classnames";
 
 const Input = ({ register, ...otherProps }: InputProps) => {
-  return <input className={s.input} {...otherProps} {...register} />;
+  return (
+    <input
+      {...otherProps}
+      className={classNames([s.input, otherProps.className])}
+      {...register}
+    />
+  );
 };
 
 export default Input;
