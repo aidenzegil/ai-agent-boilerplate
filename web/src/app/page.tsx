585aa8359e67ad22bdc966c89945f68c7ad7c5f4
Samuel Crider <sam.crider@aol.com>
2024-01-08 11:59:27 -0500

    rework link button coloring

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import MarkdownEditor from "./components/openSource/MarkdownEditor/index";
-
 export default function Home() {
-  return (
-    <main style={{ width: "100%" }}>
-      <MarkdownEditor />
-    </main>
-  );
+  return <main style={{ width: "100%" }}></main>;
 }
