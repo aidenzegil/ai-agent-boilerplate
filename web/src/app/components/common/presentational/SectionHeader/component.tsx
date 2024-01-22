26217e70c340b5768d52b12c8cc3b6047fe88fa4
SamuelRCrider <sam.crider@aol.com>
2024-01-22 13:27:39 -0500

    added breakpoint to section header for better ui

--- web/src/app/components/common/presentational/SectionHeader/component.tsx
+++ web/src/app/components/common/presentational/SectionHeader/component.tsx
@@ -5,8 +5,9 @@ import s from "./styles.module.scss";
 const SectionHeader = ({ children }: SectionHeaderProps) => {
   return (
     <div className={s.container}>
+      <div className={s.fillerLeft}></div>
       <div className={s.text}>{children}</div>
-      <div className={s.filler}></div>
+      <div className={s.fillerRight}></div>
     </div>
   );
 };
