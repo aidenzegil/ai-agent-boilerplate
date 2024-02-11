bcd6d64594f460283c469a293c4f7f48cca2b336
Aiden Zegil <aidenzegil@gmail.com>
2024-02-11 16:52:51 -0500

    HOLY

--- web/src/app/components/wrappers/ClientWrapper.tsx
+++ web/src/app/components/wrappers/ClientWrapper.tsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
 
-const ClientWrapper = <P,>(C: React.ComponentType<P>) => {
+const ClientWrapper = <P,>(Component: React.ComponentType<P>) => {
   const WrappedComponent = (props: P & JSX.IntrinsicAttributes) => {
     const [mounted, setMounted] = useState(false);
     useEffect(() => {
       setMounted(true);
     }, []);
     if (!mounted) return null;
-    return <C {...props} />;
+    return <Component C {...props} />;
   };
   return WrappedComponent;
 };
-
 
 export default ClientWrapper;
